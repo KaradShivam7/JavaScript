7ea69882fd84f543f991635a836c015f23ca06ec
KaradShivam7 <shivamkarad20@gmail.com>
2026-02-17 21:46:54 +0530

    Object

---
+++ object/Third.js
@@ -0,0 +1,20 @@
+
+
+let Obj=
+{
+    names:[{
+        sub:"Pune",
+        Post:"Baramati",
+        Area:"23345sq km"
+    },
+{
+        sub:"Dhule",
+        Post:"Amaravamati",
+        Area:"23km"
+    }],
+    population:'72 lakhs'
+}
+
+//445
+
+console.log(Obj.names[1].Post);
